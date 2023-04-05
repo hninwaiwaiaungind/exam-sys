971abd1ab04d1a4295cf3b387695222948ac7140
hninwaiwaiaung <hnin.waiwaiaung@seattleconsultingmyanmar.com>
2023-04-05 22:22:46 +0630

    commit for adding production setting

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   apiEndPoint: 'https://backend-api-2ms0.onrender.com/api',
-  apiUpload: 'https://res.cloudinary.com/dcroiiwsk',
+  apiUpload: '',
   production: true
 };
