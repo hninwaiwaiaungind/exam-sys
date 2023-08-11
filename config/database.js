6e67cb33a33a646eb0ede3cb5bb38267af466d9f
hninwaiwaiaung <hninwaiwaiaung@metateammyanmar.com>
2023-08-11 09:50:15 +0630

    change ssl connection.

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: true
+      ssl: false
     },
     debug: false
   },
